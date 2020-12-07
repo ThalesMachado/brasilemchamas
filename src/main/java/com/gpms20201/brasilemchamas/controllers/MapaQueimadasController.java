0da3e4fc4e32439ac7c2fc8abaf2411320a91960
Thales de Souza Machado <thalesm@ipiranga.ipiranga>
2020-12-07 14:32:46 -0300

    Criação da Partedo Foco de Queimadas

---
+++ src/main/java/com/gpms20201/brasilemchamas/controllers/MapaQueimadasController.java
@@ -0,0 +1,19 @@
+package com.gpms20201.brasilemchamas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mq")
+public class MapaQueimadasController {
+
+    @RequestMapping(value = { "", "/index" })
+    public String index(Model modelo) {
+
+        modelo.addAttribute("titulo", "Mapa das Queimadas");
+
+        return "MapaQueimadas/mapa.html";
+    }
+
+}
